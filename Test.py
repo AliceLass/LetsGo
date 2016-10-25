fdd1af630c4c1b6ee12f3bf2701d0a3826bc80e2
Frambaar <bastien.cohen-chalton@student.ecp.fr>
2016-10-25 15:13:55 +0200

    Commit fichier test

--- Test.py
+++ Test.py
@@ -1,16 +1,23 @@
-import autolib
-import velib
-import Itinerary
 import ItineraryDispatch
 import Point
-import requests
-import MetaItinerary
-import Exemple
 
 
-depart=Point.Point()
-depart.FromAddress("5 place du cardinal Amette")
+#print(Exemple.OriginExemple.coordinates)
 
-Meta=MetaItinerary.MetaItinerary(Exemple.OriginExemple,Exemple.ArrivalExemple)
-print(Meta.min_durationAT())
+#IT=ItineraryDispatch.ItineraryTransit(Exemple.OriginExemple,Exemple.ArrivalExemple)
+#print(IT.Transit.itinerary)
+#steps=[]
+#for i in IT.Transit.itinerary["routes"][0]["legs"][0]["steps"]:
+#    if i['travel_mode']=="TRANSIT":
+#        step={}
+#        step['departure_stop']=i['transit_details']['departure_stop']['name']
+#        step['line']=i['transit_details']['line']['short_name']
+#        step['direction']=i['transit_details']['headsign']
+#        step['type']=i['transit_details']['line']['vehicle']['name']
+#        step['arrival_stop']=i['transit_details']['arrival_stop']['name']
+#        steps.append(step)
+
+#print(steps)
+
+steps=meta.transititinerary.ItinerarySteps()
 
